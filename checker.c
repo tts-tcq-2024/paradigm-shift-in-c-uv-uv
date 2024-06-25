983b94d1794d7c38355d4a65150c066038dde8c3
Yuvaraj <yuvifeb28@gmail.com>
2024-06-25 21:03:58 +0530

    First check with 3 function

--- checker.c
+++ checker.c
@@ -1,18 +1,49 @@
 #include <stdio.h>
 #include <assert.h>
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
+int temperatureIsOk(float temperature)
+{
+  if(temperature < 0 || temperature > 45) 
+  {
     printf("Temperature out of range!\n");
     return 0;
-  } else if(soc < 20 || soc > 80) {
+  }
+  return 1;
+}
+
+int socIsOk(float soc)
+{
+  if(soc < 20 || soc > 80)
+   {
     printf("State of Charge out of range!\n");
     return 0;
-  } else if(chargeRate > 0.8) {
+  }
+  return 1;
+}
+
+int chargeRateIsOk(float chargeRate)
+{
+  if(chargeRate > 0.8)
+   {
     printf("Charge Rate out of range!\n");
     return 0;
   }
   return 1;
+}
+
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+  int temp_1 = 0;
+  int temp_2 = 0;
+  int temp_3 = 0;
+
+  temp_1 = temperatureIsOk(temperature);
+  temp_2 = temperatureIsOk(soc);
+  temp_3 = temperatureIsOk(chargeRate); 
+
+  if (temp_1 ==0 && temp_2 == 0 && temp_3 ==0)
+  {return 0;}
+  else
+  {return 1;}  
 }
 
 int main() {
