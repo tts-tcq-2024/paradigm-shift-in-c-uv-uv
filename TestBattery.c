44e1f9315b33862e944dec8323bcd1856baaa0fd
YUK5COB <K.Yuvaraj@in.bosch.com>
2024-07-28 19:23:29 +0530

    updatecase

--- TestBattery.c
+++ TestBattery.c
@@ -9,7 +9,6 @@ void testBattery()
     assert(batteryIsOk(2.25, 50, 0.5));
     assert(batteryIsOk(25, 24, 0.5));
     assert(batteryIsOk(25, 50, 0.76));
-    assert(!batteryIsOk(-1, 50, 0.5));
     assert(!batteryIsOk(25, 19, 0.5));
     assert(!batteryIsOk(25, 50, 0.81));
 }
