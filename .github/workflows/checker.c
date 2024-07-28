851cc91a7baff00ec86e8cf688f6f47054982525
YUK5COB <K.Yuvaraj@in.bosch.com>
2024-07-28 17:13:49 +0530

    Update complete logic

--- .github/workflows/checker.c
+++ .github/workflows/checker.c
@@ -1,47 +1,52 @@
 #include <stdio.h>
 #include <assert.h>
 
-int temperatureIsOk(float temperature)
+void logStatus(int isOk, const char* parameterName) 
 {
-  if(temperature < 0 || temperature > 45) 
+  if (!isOk)
   {
-    printf("Temperature out of range!\n");
-    return 0;
+      printf("%s out of range!\n", parameterName);
   }
-  else
-  {return 1;}
 }
 
-int socIsOk(float soc)
+int isValueOutOfRange(float value, float Lower_boundary, float Upper_boundary)
 {
-  if(soc < 20 || soc > 80)
-   {
-    printf("State of Charge out of range!\n");
-    return 0;
-  }
-  else
-  {return 1;}
+    return (value < Lower_boundary || value > Upper_boundary);
 }
 
-int chargeRateIsOk(float chargeRate)
+int isValueGreaterThan(float value, float threshold)
 {
-  if(chargeRate > 0.8)
-   {
-    printf("Charge Rate out of range!\n");
-    return 0;
-  }
-  else
-  {return 1;}
+    return (value > threshold);
 }
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if ((temperatureIsOk(temperature) + socIsOk(soc) + chargeRateIsOk(chargeRate) ) > 1)
-  {return 1;}
-  else
-  {return 0;}
+int IstemperatureOk(float temperature)
+{
+  int tempOk = !isValueOutOfRange(temperature, 0, 45);
+  logStatus(tempOk, "Temperature");
+  return tempOk;
 }
 
-int main() {
+int IsSocOk(float soc)
+{
+  int socOk = !isValueOutOfRange(soc, 20, 80);
+  logStatus(socOk, "State of Charge");
+  return socOk;
+}
+
+int IschargeRateOk(float chargeRate)
+{
+  int chargeRateOk = !isValueGreaterThan(chargeRate, 0.8);
+  logStatus(chargeRateOk, "Charge Rate");
+  return chargeRateOk;
+}
+
+int batteryIsOk(float temperature, float soc, float chargeRate) 
+{
+    return temperatureIsOk(temperature) && socIsOk(soc) && chargeRateIsOk(chargeRate);
+}
+
+int main() 
+{
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
