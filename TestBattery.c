54d546b48975f028acac950db83a7ecd4e6a27b5
YUK5COB <K.Yuvaraj@in.bosch.com>
2024-08-04 18:02:11 +0530

    test case update

--- TestBattery.c
+++ TestBattery.c
@@ -6,9 +6,27 @@ void testBattery()
 {
     assert(batteryIsOk(25, 70, 0.7));
     assert(!batteryIsOk(50, 85, 0));
+
+    assert(batteryIsOk(25, 70, 0.7));
+    assert(batteryIsOk(0, 20, 0.0));
+    assert(batteryIsOk(45, 80, 0.8));
+
     assert(batteryIsOk(2.25, 50, 0.5));
+    assert(batteryIsOk(42.75, 50, 0.5));
+
     assert(batteryIsOk(25, 24, 0.5));
+    assert(batteryIsOk(25, 76, 0.5));
+
     assert(batteryIsOk(25, 50, 0.76));
+
+    assert(!batteryIsOk(-1, 50, 0.5));
+    assert(!batteryIsOk(46, 50, 0.5));
+
     assert(!batteryIsOk(25, 19, 0.5));
+    assert(!batteryIsOk(25, 81, 0.5));
+
     assert(!batteryIsOk(25, 50, 0.81));
+
+    assert(!batteryIsOk(2.25, 19, 0.76));
+    assert(!batteryIsOk(42.75, 81, 0.81));
 }
